37f6e089b7b4fcdd472f1f3e8844a9f2deac253e
Chad Phillips <chad@apartmentlines.com>
2015-09-11 18:25:42 -0400

    skip kernel update, force install of linux-headers package for current kernel.

--- vagrant/vm-init.sh
+++ vagrant/vm-init.sh
@@ -86,11 +86,11 @@ vagrant up --no-provision
 
 # This is necessary so that the vagrant-vbguest plugin can be properly
 # installed.
-echo "Updating server kernel..."
-vagrant ssh -- "sudo apt-get -q -y update"
-vagrant ssh -- "sudo apt-get -q -y upgrade linux-image-amd64"
+#echo "Updating server kernel..."
+#vagrant ssh -- "sudo apt-get -q -y update"
+#vagrant ssh -- "sudo apt-get -q -y upgrade linux-image-amd64"
 echo "Ensuring gcc/make/kernel-devel are installed..."
-vagrant ssh -- "sudo apt-get -q -y install gcc make linux-kernel-headers"
+vagrant ssh -- "sudo apt-get -q -y install gcc make linux-kernel-headers linux-headers-\$(uname -r)"
 echo "Installing some useful preliminary packages"
 vagrant ssh -- "sudo apt-get -q -y install rsync vim"
 
